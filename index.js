9e9a38f5437fdccba100a52c3ba8eab0ffbdf56a
victor <mwakiretivictor@gmail.com>
2024-10-09 12:36:49 +0300

    update my arrow functions

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1) Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// 2) divide divides 2000 by 100
+console.log(divide()); // Output: 20
+
+// 3) Arrow function called square
+const square = (num) => {
+    return num * num;
+};
+
+// 4) square arrow function takes one parameter and multiplies it times itself
+console.log(square(4)); // Output: 16
+
+// 5) Arrow function called add
+const add = (a, b) => {
+    return a + b;
+};
+
+// 6) add arrow function takes two parameters and adds them together
+console.log(add(3, 5)); // Output: 8
